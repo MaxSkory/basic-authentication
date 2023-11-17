c9c647a4dc74eb3dd9cc403e858f269591a86982
Max Skory <devisrush@gmail.com>
2023-11-18 00:47:09 +0200

    init

---
+++ src/main/java/mskory/base/authentication/BaseAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package mskory.base.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mskory/base/authentication/BaseAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package mskory.base.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
